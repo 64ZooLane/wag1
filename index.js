708b27278382015d2b34bf637d6bc978cfddefe7
64ZooLane <64433470+64ZooLane@users.noreply.github.com>
2020-07-23 17:19:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,24 +8,33 @@ try
 }
 catch (e)
 {
-    console.error(e);
+    if (e.code == "ENOENT")
+    {
+        console.error("[ERR] File tokens.txt does not exist. Run setup.bat to continue");
+    }
+    else
+    {
+        console.error(e);
+    }
     return process.exit();
 }
 
 if (tokens.length >= 1)
 {
-    console.log("Attempting startup on " + tokens.length + " account(s)")
+    console.log("Attempting startup on " + tokens.length + " account(s)");
 
     tokens.forEach((t, i) =>
     {
         setTimeout(() =>
         {
+            console.log();
+            console.log(`Starting instance ${i + 1} of ${tokens.length}`);
             fork("./bot.js", ["-token", t]);
         }, i * 4000)
     })
 }
 else
 {
-    console.error("[ERR] No token provided, run ./config/token.exe to automatically set your token or refer to README.md to manual setup");
+    console.error("[ERR] No token provided. Start setup.bat to automatically set your token, or refer to README.md for manual instructions");
     return process.exit();
 }
